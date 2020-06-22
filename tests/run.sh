0fa5779f2df5b5fe64e37aea1f52acfc6b39beaa
Saulo Aflitos <saulo.aflitos@bejo.nl>
2020-06-22 16:36:23 +0200

    git ignore and less and disable debug in test.py

--- tests/run.sh
+++ tests/run.sh
@@ -1,8 +1,8 @@
 set -xeu
 
-rm -v *.tbj || true
+rm -v *.tbj *.log || true
 rm -v __pycache__ || true
 
-pypy test.py annotated_tomato_150.100000.vcf.gz.tbi
+# pypy test.py annotated_tomato_150.100000.vcf.gz.tbi 2>&1 > annotated_tomato_150.100000.vcf.gz.tbj.log
 
-# pypy test.py annotated_tomato_150.vcf.bgz.tbi
+pypy test.py annotated_tomato_150.vcf.bgz.tbi 2>&1 > annotated_tomato_150.vcf.bgz.tbj.log
